aefe50d733998cbe62582aaa7205cd99343ee662
Saurabh Saxena <saurabh@sausaxs-MacBook-Pro.local>
2017-10-25 13:47:41 -0700

    Uncommented preprocessing steps

--- python/preprocessing.py
+++ python/preprocessing.py
@@ -8,13 +8,13 @@ from update_joint_loc_for_new_origin import update_joint_location
 
 
 print("Transforming mpii mat file to json")
-#save_joints()
+save_joints()
 
 print("Saving images data that have right hand joints marked visible")
-#save_normalized_right_hand_data()
+save_normalized_right_hand_data()
 
 print("Cropping images with right elbow as center")
-#save_cropped_images()
+save_cropped_images()
 
 print("Creating cluster of poses based on inter-cluster joint distance")
 create_pose_clusters()
